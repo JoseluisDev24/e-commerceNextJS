51c484cbcc24b2535c62ecac5ab177fab89d897c
JoseluisDev24 <161613858+JoseluisDev24@users.noreply.github.com>
2025-05-04 22:22:06 -0300

    modified constats file

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,7 @@
-export const API_URL = "http://localhost:3000/api";
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api";
 
 export const API = {
-  LOGIN: API_URL + "/auth/login",
-  PRODUCTS: API_URL + "/products", 
+  LOGIN: `${API_URL}/auth/login`,
+  PRODUCTS: `${API_URL}/products`,
 };
